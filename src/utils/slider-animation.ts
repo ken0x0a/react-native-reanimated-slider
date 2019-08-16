959ca50468f730ce2c8779a5d740d158f04cfbe5
Ken <2770219+ken0x0a@users.noreply.github.com>
2019-08-17 04:37:50 +0900

    chore: clean up unused code

--- src/utils/slider-animation.ts
+++ src/utils/slider-animation.ts
@@ -70,13 +70,10 @@ export function useGestureHandleAndAnimatedStyle({
 
     const clock = new Clock()
 
-    // const edgeBehavior = [cond(lessOrEq(position, width), abs(position), sub(width * 2, position))]
-
     /**
-     * onChange `index`, `prevIndex`
+     * onChange `index`
      */
     const index = new Value<number>(0)
-    const prevIndex = new Value<number>(0)
 
     /**
      * snap
@@ -93,14 +90,10 @@ export function useGestureHandleAndAnimatedStyle({
       springConfig,
       velocityX,
       index,
-      prevIndex,
     })
 
     const translateX = block([
-      cond(eq(state, GestureState.END), [
-        // cond(clockRunning(clock), 0, startClock(clock)),
-        ...snapBehavior,
-      ]),
+      cond(eq(state, GestureState.END), snapBehavior),
       cond(eq(state, GestureState.BEGAN), [set(prevTranslationX, 0)]),
       cond(eq(state, GestureState.ACTIVE), [
         cond(clockRunning(clock), stopClock(clock)),
@@ -109,10 +102,8 @@ export function useGestureHandleAndAnimatedStyle({
         set(prevTranslationX, translationX),
       ]),
       ...(step && onIndexChange
-        ? [onChange(prevIndex, call([prevIndex], ([currentIdx]) => onIndexChange(currentIdx)))]
+        ? [onChange(index, call([index], ([currentIdx]) => onIndexChange(currentIdx)))]
         : []),
-      // cond(greaterOrEq(position, 0), position, multiply(position, -1)),
-      // ...edgeBehavior,
       position,
     ])
     const pos = interpolate(translateX, {
@@ -186,7 +177,6 @@ function getBehaviors({
   clock,
   velocityX,
   index,
-  prevIndex,
 }: Pick<
   UseGestureHandleAndAnimatedStyleArgs,
   'step' | 'onIndexChange' | 'width' | 'maxValue' | 'minValue' | 'springConfig'
@@ -194,7 +184,6 @@ function getBehaviors({
   clock: Animated.Clock
   index: Animated.Value<number>
   position: Animated.Value<number>
-  prevIndex: Animated.Value<number>
   velocityX: Animated.Value<number>
 }): { crossThresholdBehavior: Animated.Node<number>[]; snapBehavior: Animated.Node<number>[] } {
   if (!step)
@@ -225,18 +214,13 @@ function getBehaviors({
     ? [
         points.reduce<Animated.Node<number>>(
           (pv, cv, i) =>
-            cond(
-              greaterThan(position, cv - interval_d2),
-              cond(neq(prevIndex, i), set(prevIndex, i)),
-              pv,
-            ),
+            cond(greaterThan(position, cv - interval_d2), cond(neq(index, i), set(index, i)), pv),
           (0 as unknown) as Animated.Node<number>,
         ),
       ]
     : []
 
   const toValue = new Value<number>(0)
-  const point = new Value<number>(0)
 
   return {
     crossThresholdBehavior,
@@ -248,10 +232,8 @@ function getBehaviors({
           onIndexChange,
           position,
           velocityX,
-          prevIndex,
           index,
           toValue,
-          point,
           points,
           interval_d2,
         }),
@@ -270,19 +252,15 @@ function selectSnapPoint({
   onIndexChange,
   position,
   velocityX,
-  prevIndex,
   index,
   toValue,
-  point,
   points,
   interval_d2,
 }: Pick<UseGestureHandleAndAnimatedStyleArgs, 'onIndexChange'> & {
   index: Animated.Value<number>
   interval_d2: number
-  point: Animated.Value<number>
   points: number[]
   position: Animated.Value<number>
-  prevIndex: Animated.Value<number>
   toValue: Animated.Value<number>
   velocityX: Animated.Value<number>
 }) {
@@ -297,62 +275,17 @@ function selectSnapPoint({
         pv
           ? cond(
               greaterThan(estimate, cv - interval_d2),
-              [onIndexChange ? cond(neq(prevIndex, i), set(prevIndex, i)) : 0, set(toValue, cv)],
+              [onIndexChange ? cond(neq(index, i), set(index, i)) : 0, set(toValue, cv)],
               pv,
             )
           : /**
              * special case for `i === 0`,
              * as value of `estimate` might be under `points[0] - interval_d2`.
              */
-            block([
-              onIndexChange ? cond(neq(prevIndex, i), set(prevIndex, i)) : 0,
-              set(toValue, cv),
-            ]),
+            block([onIndexChange ? cond(neq(index, i), set(index, i)) : 0, set(toValue, cv)]),
       (0 as unknown) as Animated.Node<number>,
     ),
-    // ...setSnapPoints(estimate, points, toValue, prevIndex, interval_d2),
     // debug('velocityX', velocityX),
     // debug('estimate', estimate),
-    // debug('point', point),
-    // onIndexChange
-    //   ? cond(neq(prevIndex, index), [
-    //       set(prevIndex, index),
-    //       // call([index], ([currentIdx]) => onIndexChange(currentIdx)),
-    //     ])
-    //   : 0,
-    // set(toValue, point),
-  ]
-}
-function setSnapPoints(
-  estimate: Animated.Node<number>,
-  points: number[],
-  toValue: Animated.Value<number>,
-  prevIndex: Animated.Value<number>,
-  interval_d2: number,
-) {
-  const diff = new Value<number>(0)
-
-  return [
-    points.reduce<Animated.Node<number>>(
-      (pv, cv, i) =>
-        cond(
-          greaterThan(estimate, cv - interval_d2),
-          [cond(neq(prevIndex, i), set(prevIndex, i)), set(toValue, cv)],
-          pv,
-        ),
-      (0 as unknown) as Animated.Node<number>,
-    ),
-    // set(diff, abs(sub(points[0] - 1, estimate))),
-    // ...points.map((pt, i) => {
-    //   const newDiff = abs(sub(pt, estimate))
-    //   return cond(lessThan(newDiff, diff), [
-    //     // call([newDiff, diff], ([nd, d]) => {
-    //     //   console.debug({ nd, d, i, pt })
-    //     // }),
-    //     set(diff, newDiff),
-    //     set(point, pt),
-    //     set(index, i),
-    //   ])
-    // }),
   ]
 }
