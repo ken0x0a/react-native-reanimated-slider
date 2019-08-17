45405551d8f1251b6e57e8322639bc0c1e262943
Ken <2770219+ken0x0a@users.noreply.github.com>
2019-08-17 15:41:15 +0900

    chore: update default color

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,6 +1,6 @@
 export const colors = {
-  orange: '#FF653A',
-  orangeLight: '#FFC3B2',
+  orange: '#EF755A',
+  orangeLight: '#FFE3B2',
   lightGray: '#F1F1F1',
   white: '#FFF',
   shadow: '#7c7c7c80',
